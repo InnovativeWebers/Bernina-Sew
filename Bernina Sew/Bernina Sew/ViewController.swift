646949c5c59b60bcd3e16c07008b768bea9c0921
CHENG-LIANG1 <48384264+CHENG-LIANG1@users.noreply.github.com>
2021-04-12 21:50:30 +0800

    Bernina Sew App initial set up

---
+++ Bernina Sew/Bernina Sew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bernina Sew
+//
+//  Created by lunarian on 01/06/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
